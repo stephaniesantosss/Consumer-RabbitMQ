44f52b3737c10e217f616592977bd043d9dcf960
stephaniesantosss <stephanieps2016@hotmail.com>
2022-04-03 12:26:13 -0300

    Criando dto e adicionando lombok ao projeto

---
+++ src/main/java/com/example/spring/consumer/dto/Message.java
@@ -0,0 +1,15 @@
+package com.example.spring.consumer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private String text;
+}
